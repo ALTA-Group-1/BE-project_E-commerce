4f83930db50e27366671e2944a25954939873cfc
Muhammad Jauhari <mhmd.jauhari@gmail.com>
2022-09-13 12:00:47 +0700

    setup handle CORS

--- main.go
+++ main.go
@@ -20,6 +20,12 @@ func main() {
 	e := echo.New()
 	e.Pre(middleware.RemoveTrailingSlash())
 
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+	}))
+
 	factory.InitFactory(e, db)
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", cfg.SERVER_PORT)))
