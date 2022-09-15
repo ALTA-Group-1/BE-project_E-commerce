dcc4a8f158872eb506e6b5106a7c73fd4198d342
Dian Nurdiana <diannd1507@gmail.com>
2022-09-15 18:43:45 +0700

    fix update increment

--- features/cart/delivery/handler.go
+++ features/cart/delivery/handler.go
@@ -19,7 +19,7 @@ func New(e *echo.Echo, usecase cart.UsecaseInterface) {
 	}
 	e.POST("/carts", handler.PostData, middlewares.JWTMiddleware())
 	e.GET("/carts", handler.GetAllCart, middlewares.JWTMiddleware())
-	e.PUT("/carts/increment/:id", handler.UpdatePlus, middlewares.JWTMiddleware())
+	e.PUT("/carts/:id", handler.UpdatePlus, middlewares.JWTMiddleware())
 	e.PUT("/carts/:id/decrement", handler.UpdateMinus, middlewares.JWTMiddleware())
 	e.DELETE("/carts/:id", handler.DeleteCart, middlewares.JWTMiddleware())
 }
@@ -98,7 +98,9 @@ func (delivery *CartDelivery) UpdatePlus(c echo.Context) error {
 		return c.JSON(400, helper.FailedResponseHelper("error bind data"))
 	}
 
-	row, err := delivery.cartUsecase.UpdatePlus(idCart, "increment")
+	query := c.QueryParam("increment")
+
+	row, err := delivery.cartUsecase.UpdatePlus(idCart, query)
 	if err != nil || row < 1 {
 		return c.JSON(400, helper.FailedResponseHelper("Bad Request"))
 	}
